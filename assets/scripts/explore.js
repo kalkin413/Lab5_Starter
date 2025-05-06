91ef809e0735805ce0744f4de4aed4e9c80a9369
kalkulator413 <kalkin413@gmail.com>
2025-05-05 20:46:52 -0700

    finish explore pt1

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -4,4 +4,40 @@ window.addEventListener('DOMContentLoaded', init);
 
 function init() {
   // TODO
+  const synth = window.speechSynthesis;
+  const dropDown = document.querySelector("select");
+  const textArea = document.querySelector('#text-to-speak');
+  const img = document.querySelector("img");
+  
+  // populate list of voices
+  const voices = synth.getVoices();
+  for (let i = 0; i < voices.length; i++) {
+    const curr = document.createElement("option");
+    curr.textContent = voices[i].name;
+    dropDown.appendChild(curr);
+  }
+
+  const button = document.querySelector("button");
+  button.addEventListener('click', function() {
+    // grab text contents
+    let text = textArea.value;
+
+    // make synthesizer speak & change image
+    const toSpeak = new SpeechSynthesisUtterance(text);
+    for (let i = 0; i < voices.length; i++) {
+      if (dropDown.value == voices[i].name) {
+        toSpeak.voice = voices[i];
+      }
+    }
+    
+    synth.speak(toSpeak);
+  });
+
+  setInterval(function() {
+    if (synth.speaking) {
+      img.src = 'assets/images/smiling-open.png';
+    } else {
+      img.src = 'assets/images/smiling.png';
+    }
+  }, 100);
 }
